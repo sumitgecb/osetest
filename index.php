7a3075790f4cfa0dd17a18ff28e4ba6dc7b886ef
sumitgecb <sumitgecb@gmail.com>
2021-01-31 01:17:55 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-Print "Welcome to OSE, Hitesh Apki help sai hum OSE ko welocme kr pa rhe hai hum iske bahut bahut abhari rhenge"
+print "Welcome to OSE, Hitesh Apki help sai hum OSE ko welocme kr pa rhe hai hum iske bahut bahut abhari rhenge"
 print "Thanks and Regards"
 print "Sumit Agrawal"
 
