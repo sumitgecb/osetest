0b02f43c5be4f74732f7e2e31b284022acc9b153
sumitgecb <sumitgecb@gmail.com>
2021-02-02 21:25:27 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
-print "Welcome to OSE, Hitesh Apki help sai hum OSE ko welocme kr pa rhe hai hum iske bahut bahut abhari rhenge"
+print "Welcome to OSE, Hitesh Apki help sai hum OSE ko welocme kr pa rhe hai hum iske bahut bahut abhari rhenge";
+print `ifconfig`;
 ?>
